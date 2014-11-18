c412a273d7a59f3d118ebf9da9c92f39da25e1dd
resteve <resteve@zikzakmedia.com>
2014-11-18 08:59:53 +0100

    FIX header module

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
-#This file is part helloword module for Tryton.
+#This file is part party_country module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
 from .test_party_country import suite
