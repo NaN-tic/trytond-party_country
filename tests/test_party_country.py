c84f301b3fc756f178ddfb1725855c91f821aafa
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-06-21 16:53:02 +0200

    Update to latest version

--- tests/test_party_country.py
+++ tests/test_party_country.py
@@ -1,28 +1,17 @@
-#This file is part party_country module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class PartyCountryTestCase(unittest.TestCase):
+class PartyCountryTestCase(ModuleTestCase):
     '''Test Party Country module.'''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('party_country')
-
-    def test0005views(self):
-        '''Test views'''
-        test_view('party_country')
-
-    def test0006depends(self):
-        '''Test depends'''
-        test_depends()
+    module = 'party_country'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        CountryZipTestCase))
+        PartyCountryTestCase))
     return suite
